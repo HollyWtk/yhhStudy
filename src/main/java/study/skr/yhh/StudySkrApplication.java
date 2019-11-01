7980dbdc211d8ad9cec3bdedd09e38a1a2e9b6ec
yhh <hollywtk@163.com>
2019-11-01 18:30:55 +0800

    1

---
+++ src/main/java/study/skr/yhh/StudySkrApplication.java
@@ -0,0 +1,13 @@
+package study.skr.yhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/skr/yhh/StudySkrApplicationTests.java
@@ -0,0 +1,13 @@
+package study.skr.yhh;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
